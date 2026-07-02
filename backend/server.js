7e2830d0084631205ade62fb93c77bc1dc0c1a38
ori-Balajee <balajee4drip@gmail.com>
2026-07-03 03:17:25 +0530

    Case bugging

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
-const Term = require('./models/Term');
+const Term = require('./Models/Term');
 require('dotenv').config();
 
 // Route Groups
